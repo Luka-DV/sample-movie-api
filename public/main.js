1151f4486fb90b4dc4c0b383b2e29d7f7f7e4612
Luka-DV <luka.guva@gmail.com>
2024-08-19 18:36:08 +0200

    add logic for more than one or zero movies

--- public/main.js
+++ public/main.js
@@ -8,15 +8,35 @@ async function findMovieApiReq() { {
         const response = await fetch(`https://sample-movie-api.onrender.com/api/${movieTitle}`);
         const data = await response.json();
 
-        console.log(data)
+        console.log("DATA: ", data);
 
+        //if the data array is empty
+        if(!data.length) {
+            document.querySelector("#move-title").textContent = "";
+            document.querySelector("#release-year").textContent = "";
+            document.querySelector("#genre").textContent = "";
+
+            document.querySelector("#additional").textContent = `Didn't find any movies with this title.`;
+            return;
+        }
+
+        //if the data array isnt empty
         document.querySelector("#move-title").textContent = `Movie title: ${data[0].title}`;
         document.querySelector("#release-year").textContent = `Year: ${data[0].year}`;
         document.querySelector("#genre").textContent = `Genres: ${data[0].genres}`
 
+        //if there is more than one item in the data array
+        if(data.length > 1) {
+            if(data.length === 2) {
+                document.querySelector("#additional").textContent = `Found one more movie.`
+            } else {
+                document.querySelector("#additional").textContent = `Found ${data.length - 1} more movies.`
+            }
+        }
+
 
     } catch (error) {
-        console.error(error);
+        console.error("Error: ", error);
     }
 }
     
